4e42ac9ef4f4a953fd1da98901c10fdb44bcee2b
Sven Le Cann <sven.lecann@live.fr>
2020-09-09 00:27:35 +0200

    Section 8 - 139

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,6 +5,10 @@ interface UserAttrs {
   password: string;
 }
 
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -16,10 +20,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+User.build({
+  email: 'test@test.com',
+  password: 'password',
+});
+
+export { User };
